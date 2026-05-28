3dfbd552acb78125469cfd1fdb7b4d13c54ea3f2
SANTZSPAXX <lsd.contanova@gmail.com>
2026-05-28 11:47:21 -0300

    chore: adicionado basePath /jundiai e metadados estruturais

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,22 @@
+/**
+ * @company Kore Nexus
+ * @version 1.0.2
+ * @last_update 2026-05-28 11:46:00
+ * @author Guilherme Santiago
+ * @changelog
+ * - Injeção da propriedade basePath para responder em /jundiai
+ * - Preservação das configurações de ignoreBuildErrors e unoptimized images
+ */
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/jundiai',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
